5124d45c711f372fc9519ce702c63b979a3a085e
Sriyans Kanneganti <srkanneg@calpoly.edu>
2026-06-05 11:58:33 -0700

    Used parquet

--- R/load_data.R
+++ R/load_data.R
@@ -6,9 +6,9 @@
 #' @return A tibble with columns including `country`, `year`, `volunteers`,
 #'   `grand_total`, and plastic type columns (hdpe, ldpe, o, pet, pp, ps, pvc).
 #'
-#' @importFrom readr read_csv
+#' @importFrom arrow read_parquet
 #' @export
 load_data <- function() {
-  path <- system.file("extdata", "plastics.csv", package = "tidyplastic")
-  readr::read_csv(path, show_col_types = FALSE)
+  path <- system.file("extdata", "plastics.parquet", package = "tidyplastic")
+  read_parquet(path)
 }
